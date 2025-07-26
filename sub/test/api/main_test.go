df7971bfa1ba62345be1b9a5e355f9f9eeae72f3
velosypedno <klyuchka.artur@gmail.com>
2025-07-26 19:47:58 +0300

    fix: prevent resource leakage in integration tests

--- sub/test/api/main_test.go
+++ sub/test/api/main_test.go
@@ -12,46 +12,81 @@ import (
 	"testing"
 	"time"
 
+	"github.com/GenesisEducationKyiv/software-engineering-school-5-0-velosypedno/pkg/messaging"
 	pb "github.com/GenesisEducationKyiv/software-engineering-school-5-0-velosypedno/proto/sub/v1alpha2"
 	"github.com/GenesisEducationKyiv/software-engineering-school-5-0-velosypedno/sub/internal/app"
 	"github.com/GenesisEducationKyiv/software-engineering-school-5-0-velosypedno/sub/internal/config"
 	_ "github.com/lib/pq"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 const apiURL = "http://127.0.0.1:8081"
 
-var DB *sql.DB
 var SubGRPCClient pb.SubscriptionServiceClient
 
+var (
+	DB            *sql.DB
+	RMQConnection *amqp.Connection
+	RMQChannel    *amqp.Channel
+	GRPCConn      *grpc.ClientConn
+)
+
+func closeConnections() {
+	if GRPCConn != nil {
+		if err := GRPCConn.Close(); err != nil {
+			log.Println("failed to close gRPC conn:", err)
+		}
+	}
+	if RMQChannel != nil {
+		if err := RMQChannel.Close(); err != nil {
+			log.Println("failed to close RMQ channel:", err)
+		}
+	}
+	if RMQConnection != nil {
+		if err := RMQConnection.Close(); err != nil {
+			log.Println("failed to close RMQ conn:", err)
+		}
+	}
+	if DB != nil {
+		if err := DB.Close(); err != nil {
+			log.Println("failed to close DB:", err)
+		}
+	}
+}
+
 func TestMain(m *testing.M) {
+
 	// setup config
 	cfg, err := config.Load()
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 	fmt.Println(cfg)
 
-	// setup DB
-	db, err := sql.Open(cfg.DB.Driver, cfg.DB.DSN())
+	// setup messaging
+	RMQConnection, RMQChannel, err = setupRMQ(cfg.RabbitMQ)
 	if err != nil {
-		log.Fatal(err)
+		closeConnections()
+		log.Panic(err)
 	}
-	DB = db
-	defer func() {
-		if err := db.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
+
+	// setup DB
+	DB, err = sql.Open(cfg.DB.Driver, cfg.DB.DSN())
+	if err != nil {
+		closeConnections()
+		log.Panic(err)
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	// run app
 	app := app.New(cfg)
 	go func() {
-		err = app.Run(ctx)
-		if err != nil {
-			log.Fatal(err)
+		runErr := app.Run(ctx)
+		if runErr != nil {
+			closeConnections()
+			log.Panic(runErr)
 		}
 	}()
 
@@ -66,30 +101,77 @@ func TestMain(m *testing.M) {
 		}
 
 		if time.Now().After(deadline) {
-			log.Fatalf("gRPC server did not become ready within 3 second at %s", cfg.GRPCSrv.Addr())
+			log.Panicf("gRPC server did not become ready within 3 second at %s", cfg.GRPCSrv.Addr())
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
 
 	// setup grpc client
 	opt := grpc.WithTransportCredentials(insecure.NewCredentials())
-	conn, err := grpc.NewClient(cfg.GRPCSrv.Addr(), opt)
+	GRPCConn, err = grpc.NewClient(cfg.GRPCSrv.Addr(), opt)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
-	defer conn.Close()
-	SubGRPCClient = pb.NewSubscriptionServiceClient(conn)
+	SubGRPCClient = pb.NewSubscriptionServiceClient(GRPCConn)
 
 	// run tests
 	code := m.Run()
 	cancel()
-
+	closeConnections()
 	os.Exit(code)
 }
 
 func clearDB() {
 	_, err := DB.Exec("TRUNCATE subscriptions")
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
+}
+
+func setupRMQ(cfg config.RabbitMQConfig) (*amqp.Connection, *amqp.Channel, error) {
+	conn, err := amqp.Dial(cfg.Addr())
+	if err != nil {
+		return nil, nil, err
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		return conn, nil, err
+	}
+
+	err = ch.ExchangeDeclare(
+		messaging.ExchangeName,
+		"direct",
+		true,  // durable
+		false, // auto-deleted
+		false, // internal
+		false, // no-wait
+		nil,   // args
+	)
+	if err != nil {
+		return conn, ch, err
+	}
+
+	q, err := ch.QueueDeclare(
+		messaging.SubscribeQueueName, // name
+		true,                         // durable
+		false,                        // delete when unused
+		false,                        // exclusive
+		false,                        // no-wait
+		nil,                          // arguments
+	)
+	if err != nil {
+		return conn, ch, err
+	}
+
+	err = ch.QueueBind(
+		q.Name,                        // queue name
+		messaging.SubscribeRoutingKey, // routing key
+		messaging.ExchangeName,        // exchange
+		false,
+		nil)
+	if err != nil {
+		return conn, ch, err
+	}
+
+	return conn, ch, nil
 }
