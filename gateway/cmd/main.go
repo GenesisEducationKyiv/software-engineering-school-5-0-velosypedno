b04e42b102587af34c1299b6b0a60dbd77ee4b65
velosypedno <klyuchka.artur@gmail.com>
2025-07-28 17:02:29 +0300

    chore: add zap logger

--- gateway/cmd/main.go
+++ gateway/cmd/main.go
@@ -2,25 +2,45 @@ package main
 
 import (
 	"context"
-	"log"
 	"os/signal"
 	"syscall"
 
 	"github.com/GenesisEducationKyiv/software-engineering-school-5-0-velosypedno/gateway/internal/app"
 	"github.com/GenesisEducationKyiv/software-engineering-school-5-0-velosypedno/gateway/internal/config"
+	"go.uber.org/zap"
 )
 
 func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		err := logger.Sync()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	logger = logger.With(zap.String("service", "gateway"))
+	mainLogger := logger.With(zap.String("component", "main"))
+
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
 	defer cancel()
 
+	mainLogger.Info("Loading config...")
 	cfg, err := config.Load()
 	if err != nil {
-		log.Panic(err)
+		mainLogger.Panic("Failed to load config", zap.Error(err))
 	}
+	mainLogger.Info("Config loaded")
+
+	mainLogger.Info("Creating app...")
 	app := app.New(cfg)
+	mainLogger.Info("App created")
+
+	mainLogger.Info("Running app...")
 	err = app.Run(ctx)
 	if err != nil {
-		log.Panic(err)
+		mainLogger.Panic("Failed to run app", zap.Error(err))
 	}
 }
